f153c0d6568d40f1f98808aa4d1520140adf6651
usmcfiredog <45340804+usmcfiredog@users.noreply.github.com>
2020-01-10 19:32:59 -0800

    update

--- CiscoExam2.py
+++ CiscoExam2.py
@@ -49,137 +49,203 @@ for qaItem in qaList[:int(numOfQs)]:
         print('\nPlease enter the number of your first answer:')
         userAnsw = input()
         while not userAnsw.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw = input()
         userAnsw = int(userAnsw)
         print('Please enter the number of your second answer:')
         userAnsw2 = input()
         while not userAnsw2.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw2 = input()
         userAnsw2 = int(userAnsw2)
         print('Please enter the number of your third answer:')
         userAnsw3 = input()
         while not userAnsw3.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw3 = input()
         userAnsw3 = int(userAnsw3)
         print('Please enter the number of your fourth answer:')
         userAnsw4 = input()
         while not userAnsw4.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw4 = input()
         userAnsw4 = int(userAnsw4)
         print('Please enter the number of your fifth answer:')
         userAnsw5 = input()
         while not userAnsw5.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw5 = input()
         userAnsw5 = int(userAnsw5)
-        if (possible[userAnsw - 1] == qaItem.corrAnsw or possible[userAnsw - 1] == qaItem.corrAnsw2 or possible[userAnsw - 1] == qaItem.corrAnsw3 or possible[userAnsw - 1] == qaItem.corrAnsw4 or possible[userAnsw - 1] == qaItem.corrAnsw5) and \
-            (possible[userAnsw2 - 1] == qaItem.corrAnsw or possible[userAnsw2 - 1] == qaItem.corrAnsw2 or possible[userAnsw2 - 1] == qaItem.corrAnsw3 or possible[userAnsw2 - 1] == qaItem.corrAnsw4 or possible[userAnsw2 - 1] == qaItem.corrAnsw5) and \
-                (possible[userAnsw3 - 1] == qaItem.corrAnsw or possible[userAnsw3 - 1] == qaItem.corrAnsw2 or possible[userAnsw3 - 1] == qaItem.corrAnsw3 or possible[userAnsw3 - 1] == qaItem.corrAnsw4 or possible[userAnsw3 - 1] == qaItem.corrAnsw5) and \
-            (possible[userAnsw4 - 1] == qaItem.corrAnsw or possible[userAnsw4 - 1] == qaItem.corrAnsw2 or possible[userAnsw4 - 1] == qaItem.corrAnsw3 or possible[userAnsw4 - 1] == qaItem.corrAnsw4 or possible[userAnsw4 - 1] == qaItem.corrAnsw5) and \
-                (possible[userAnsw5 - 1] == qaItem.corrAnsw or possible[userAnsw5 - 1] == qaItem.corrAnsw2 or possible[userAnsw5 - 1] == qaItem.corrAnsw3 or possible[userAnsw5 - 1] == qaItem.corrAnsw4 or possible[userAnsw5 - 1] == qaItem.corrAnsw5):
+        if (
+            possible[userAnsw - 1] == qaItem.corrAnsw or possible[
+                userAnsw - 1] == qaItem.corrAnsw2 or possible[
+                    userAnsw - 1] == qaItem.corrAnsw3 or possible[
+                        userAnsw - 1] == qaItem.corrAnsw4 or possible[
+                            userAnsw - 1] == qaItem.corrAnsw5) and \
+            (possible[userAnsw2 - 1] == qaItem.corrAnsw or possible[
+                userAnsw2 - 1] == qaItem.corrAnsw2 or possible[
+                    userAnsw2 - 1] == qaItem.corrAnsw3 or possible[
+                        userAnsw2 - 1] == qaItem.corrAnsw4 or possible[
+                            userAnsw2 - 1] == qaItem.corrAnsw5) and \
+                (possible[userAnsw3 - 1] == qaItem.corrAnsw or possible[
+                    userAnsw3 - 1] == qaItem.corrAnsw2 or possible[
+                        userAnsw3 - 1] == qaItem.corrAnsw3 or possible[
+                            userAnsw3 - 1] == qaItem.corrAnsw4 or possible[
+                                userAnsw3 - 1] == qaItem.corrAnsw5) and \
+            (possible[userAnsw4 - 1] == qaItem.corrAnsw or possible[
+                userAnsw4 - 1] == qaItem.corrAnsw2 or possible[
+                    userAnsw4 - 1] == qaItem.corrAnsw3 or possible[
+                        userAnsw4 - 1] == qaItem.corrAnsw4 or possible[
+                            userAnsw4 - 1] == qaItem.corrAnsw5) and \
+                (possible[userAnsw5 - 1] == qaItem.corrAnsw or possible[
+                    userAnsw5 - 1] == qaItem.corrAnsw2 or possible[
+                        userAnsw5 - 1] == qaItem.corrAnsw3 or possible[
+                            userAnsw5 - 1] == qaItem.corrAnsw4 or possible[
+                                userAnsw5 - 1] == qaItem.corrAnsw5):
             print(f'{qCorrectColor}Your answer was correct.{endColor}')
             corrCount += 1
         else:
             print(f'{qWrongColor}Your answer was wrong.{endColor}')
-            print(
-                f'Correct answers were: {corrAnsws + qaItem.corrAnsw + endColor}, {corrAnsws + qaItem.corrAnsw2 + endColor}, {corrAnsws + qaItem.corrAnsw3 + endColor}, {corrAnsws + qaItem.corrAnsw4 + endColor}, and {corrAnsws + qaItem.corrAnsw5 + endColor}')
+            print('Correct answers were: '
+                  f'{corrAnsws + qaItem.corrAnsw + endColor}, '
+                  f'{corrAnsws + qaItem.corrAnsw2 + endColor}, '
+                  f'{corrAnsws + qaItem.corrAnsw3 + endColor}, '
+                  f'{corrAnsws + qaItem.corrAnsw4 + endColor}, and '
+                  f'{corrAnsws + qaItem.corrAnsw5 + endColor}')
         print('')
     elif qaItem.corrAnsw4 is not None:
         print('\nPlease enter the number of your first answer:')
         userAnsw = input()
         while not userAnsw.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw = input()
         userAnsw = int(userAnsw)
         print('Please enter the number of your second answer:')
         userAnsw2 = input()
         while not userAnsw2.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw2 = input()
         userAnsw2 = int(userAnsw2)
         print('Please enter the number of your third answer:')
         userAnsw3 = input()
         while not userAnsw3.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw3 = input()
         userAnsw3 = int(userAnsw3)
         print('Please enter the number of your fourth answer:')
         userAnsw4 = input()
         while not userAnsw4.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw4 = input()
         userAnsw4 = int(userAnsw4)
-        if (possible[userAnsw - 1] == qaItem.corrAnsw or possible[userAnsw - 1] == qaItem.corrAnsw2 or possible[userAnsw - 1] == qaItem.corrAnsw3 or possible[userAnsw - 1] == qaItem.corrAnsw4) and \
-            (possible[userAnsw2 - 1] == qaItem.corrAnsw or possible[userAnsw2 - 1] == qaItem.corrAnsw2 or possible[userAnsw2 - 1] == qaItem.corrAnsw3 or possible[userAnsw2 - 1] == qaItem.corrAnsw4) and \
-                (possible[userAnsw3 - 1] == qaItem.corrAnsw or possible[userAnsw3 - 1] == qaItem.corrAnsw2 or possible[userAnsw3 - 1] == qaItem.corrAnsw3 or possible[userAnsw3 - 1] == qaItem.corrAnsw4) and \
-                (possible[userAnsw4 - 1] == qaItem.corrAnsw or possible[userAnsw4 - 1] == qaItem.corrAnsw2 or possible[userAnsw4 - 1] == qaItem.corrAnsw3 or possible[userAnsw4 - 1] == qaItem.corrAnsw4):
+        if (possible[userAnsw - 1] == qaItem.corrAnsw or
+            possible[userAnsw - 1] == qaItem.corrAnsw2 or
+            possible[userAnsw - 1] == qaItem.corrAnsw3 or
+            possible[userAnsw - 1] == qaItem.corrAnsw4) and \
+            (possible[userAnsw2 - 1] == qaItem.corrAnsw or
+             possible[userAnsw2 - 1] == qaItem.corrAnsw2 or
+             possible[userAnsw2 - 1] == qaItem.corrAnsw3 or
+             possible[userAnsw2 - 1] == qaItem.corrAnsw4) and \
+                (possible[userAnsw3 - 1] == qaItem.corrAnsw or
+                 possible[userAnsw3 - 1] == qaItem.corrAnsw2 or
+                 possible[userAnsw3 - 1] == qaItem.corrAnsw3 or
+                 possible[userAnsw3 - 1] == qaItem.corrAnsw4) and \
+                (possible[userAnsw4 - 1] == qaItem.corrAnsw or
+                 possible[userAnsw4 - 1] == qaItem.corrAnsw2 or
+                 possible[userAnsw4 - 1] == qaItem.corrAnsw3 or
+                 possible[userAnsw4 - 1] == qaItem.corrAnsw4):
             print(f'{qCorrectColor}Your answer was correct.{endColor}')
             corrCount += 1
         else:
             print(f'{qWrongColor}Your answer was wrong.{endColor}')
-            print(
-                f'Correct answer was: {corrAnsws + qaItem.corrAnsw + endColor}, {corrAnsws + qaItem.corrAnsw2 + endColor}, {corrAnsws + qaItem.corrAnsw3 + endColor}, and {corrAnsws + qaItem.corrAnsw4 + endColor}')
+            print('Correct answer was: '
+                  f'{corrAnsws + qaItem.corrAnsw + endColor}, '
+                  f'{corrAnsws + qaItem.corrAnsw2 + endColor}, '
+                  f'{corrAnsws + qaItem.corrAnsw3 + endColor}, and '
+                  f'{corrAnsws + qaItem.corrAnsw4 + endColor}')
         print('')
     elif qaItem.corrAnsw3 is not None:
         print('\nPlease enter the number of your first answer:')
         userAnsw = input()
         while not userAnsw.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw = input()
         userAnsw = int(userAnsw)
         print('Please enter the number of your second answer:')
         userAnsw2 = input()
         while not userAnsw2.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw2 = input()
         userAnsw2 = int(userAnsw2)
         print('Please enter the number of your third answer:')
         userAnsw3 = input()
         while not userAnsw3.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw3 = input()
         userAnsw3 = int(userAnsw3)
-        if (possible[userAnsw - 1] == qaItem.corrAnsw or possible[userAnsw - 1] == qaItem.corrAnsw2 or possible[userAnsw - 1] == qaItem.corrAnsw3) and \
-            (possible[userAnsw2 - 1] == qaItem.corrAnsw or possible[userAnsw2 - 1] == qaItem.corrAnsw2 or possible[userAnsw2 - 1] == qaItem.corrAnsw3) and \
-                (possible[userAnsw3 - 1] == qaItem.corrAnsw or possible[userAnsw3 - 1] == qaItem.corrAnsw2 or possible[userAnsw3 - 1] == qaItem.corrAnsw3):
+        if (possible[userAnsw - 1] == qaItem.corrAnsw or
+            possible[userAnsw - 1] == qaItem.corrAnsw2 or
+            possible[userAnsw - 1] == qaItem.corrAnsw3) and \
+            (possible[userAnsw2 - 1] == qaItem.corrAnsw or
+             possible[userAnsw2 - 1] == qaItem.corrAnsw2 or
+             possible[userAnsw2 - 1] == qaItem.corrAnsw3) and \
+                (possible[userAnsw3 - 1] == qaItem.corrAnsw or
+                 possible[userAnsw3 - 1] == qaItem.corrAnsw2 or
+                 possible[userAnsw3 - 1] == qaItem.corrAnsw3):
             print(f'{qCorrectColor}Your answer was correct.{endColor}')
             corrCount += 1
         else:
             print(f'{qWrongColor}Your answer was wrong.{endColor}')
-            print(
-                f'Correct answer was: {corrAnsws + qaItem.corrAnsw + endColor}, {corrAnsws + qaItem.corrAnsw2 + endColor}, and {corrAnsws + qaItem.corrAnsw3 + endColor}')
+            print('Correct answer was: '
+                  f'{corrAnsws + qaItem.corrAnsw + endColor}, '
+                  f'{corrAnsws + qaItem.corrAnsw2 + endColor}, and '
+                  f'{corrAnsws + qaItem.corrAnsw3 + endColor}')
         print('')
     elif qaItem.corrAnsw2 is not None:
         print('\nPlease enter the number of your first answer:')
         userAnsw = input()
         while not userAnsw.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw = input()
         userAnsw = int(userAnsw)
         print('Please enter the number of your second answer:')
         userAnsw2 = input()
         while not userAnsw2.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw2 = input()
         userAnsw2 = int(userAnsw2)
-        if (possible[userAnsw - 1] == qaItem.corrAnsw or possible[userAnsw - 1] == qaItem.corrAnsw2) and \
-                (possible[userAnsw2 - 1] == qaItem.corrAnsw or possible[userAnsw2 - 1] == qaItem.corrAnsw2):
+        if (possible[userAnsw - 1] == qaItem.corrAnsw or
+            possible[userAnsw - 1] == qaItem.corrAnsw2) and \
+                (possible[userAnsw2 - 1] == qaItem.corrAnsw or
+                 possible[userAnsw2 - 1] == qaItem.corrAnsw2):
             print(f'{qCorrectColor}Your answer was correct.{endColor}')
             corrCount += 1
         else:
             print(f'{qWrongColor}Your answer was wrong.{endColor}')
-            print(
-                f'Correct answer was: {corrAnsws + qaItem.corrAnsw + endColor}, and {corrAnsws + qaItem.corrAnsw2 + endColor}')
+            print('Correct answer was: '
+                  f'{corrAnsws + qaItem.corrAnsw + endColor}, and '
+                  f'{corrAnsws + qaItem.corrAnsw2 + endColor}')
         print('')
     else:
         print('\nPlease enter the number of your answer:')
         userAnsw = input()
         while not userAnsw.isdigit():
-            print('That was not a number. Please enter the number of your answer:')
+            print('That was not a number. '
+                  'Please enter the number of your answer:')
             userAnsw = input()
         userAnsw = int(userAnsw)
         if possible[userAnsw - 1] == qaItem.corrAnsw:
@@ -187,28 +253,12 @@ for qaItem in qaList[:int(numOfQs)]:
             corrCount += 1
         else:
             print(f'{qWrongColor}Your answer was wrong.{endColor}')
-            print(
-                f'Correct answer was: {corrAnsws + qaItem.corrAnsw + endColor}')
+            print('Correct answer was: '
+                  f'{corrAnsws + qaItem.corrAnsw + endColor}')
         print('')
     qnumber += 1
 
 print(
-    f'You answered {str(corrCount)} out of {int(numOfQs)} questions correctly.')
+    f'You answered {str(corrCount)} out of {int(numOfQs)} questions correctly.'
+)
 print(f'An approximate score of {int(corrCount/int(numOfQs)*1000)}')
-
-
-# SPARE QUESTION
-#    QA('Changes were made to the ISE server while troubleshooting, and now all wireless certificate authentications are failing. Logs indicate an EAP failure.\n'
-#         'What is the most likely cause of the problem?',
-#         ['Certificate authentication profile is not configured in the Identity Store',
-#            'MS-CHAPv2-is not checked in the Allowed Protocols list',
-#            'Default rule denies all traffic',
-#            'Client root certificate is not included in the Certificate '
-#            'Store'],
-#         'EAP-TLS is not checked in the Allowed Protocols list'),
-# QA('Which two things must be verified if authentication is failing with this error message? (Choose two.)',
-#     ['Cisco ISE HTTPS/admin certificate is valid.',
-#      'CA cert chain of the client certificate is installed on Cisco ISE.',
-#      'Cisco ISE server certificate is installed on the client.'],
-#     'Cisco ISE EAP identity certificate is valid.',
-#     'CA cert chain of Cisco ISE EAP certificate is installed on the trusted certs store of the client machine.'),
